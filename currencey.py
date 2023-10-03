8d54ff1f7a6ae6b535939454ce5c99c2101e8496
Biruk Bizuayehu <101051916+brook1167@users.noreply.github.com>
2023-10-03 10:29:36 +0300

    Add files via upload
    Currency App
    
    used to check the latest currency exchange from the exchange API
    
    uses the Python http request module

---
+++ currencey.py
@@ -0,0 +1,32 @@
+import json
+import requests
+from requests import HTTPError
+
+def convertor():
+  Action_command = input('Enter your choice ')
+
+  if Action_command == 'c'.lower():
+    fromInput = str(input('Enter from input '))
+    toInput = str(input('Enter your to input '))
+    amount = int(input('Enter amount '))
+
+    url = f"https://v6.exchangerate-api.com/v6/901106dca3165dc1c69db84b/pair/{fromInput}/{toInput}/{amount}"
+    try:
+      response = requests.get(url)
+      response.raise_for_status()
+      result = response.json()
+    except HTTPError as http_error:
+     print(http_error)
+    else:
+     print(
+       f"\n{amount} {fromInput.upper()} is {toInput.upper()} {result['conversion_rate'] * amount} at {result['time_last_update_utc']}\n")
+  else:
+    exit()
+
+while True:
+  print('----------- Currency Converter App-------------')
+  print('PRESS C TO USE CURRENCY CONVERTER APP')
+  print('PRESS ANY KEY TO EXIT')
+  print('-------------------------')
+  convertor()
+
